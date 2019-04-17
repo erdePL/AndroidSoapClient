496e48d185484031e65f28b4a0042173806444f7
erdePL <erde@op.pl>
2019-04-17 11:19:25 +0200

    SoapCaller- inner class of MainActivity, changed to outer class -SoapCallerIntentService. Now being called by main activity and communicate trough LocalBroadcastManager

--- app/src/main/java/com/example/kamil/androidsoapclient2/activity/MainActivity.java
+++ app/src/main/java/com/example/kamil/androidsoapclient2/activity/MainActivity.java
@@ -13,9 +13,10 @@ import android.widget.TextView;
 import com.example.kamil.androidsoapclient2.R;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
-    Button button;
+    private TextView textView;
+    private Button button;
     public static final String INTENT_FILTER_SOAP_CALLER = "intent-filter-soap-caller";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,103 +24,21 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
         button = (Button) findViewById(R.id.button);
 
-        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
-                new IntentFilter(INTENT_FILTER_SOAP_CALLER));
-
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter(INTENT_FILTER_SOAP_CALLER));
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent soapCallerIntent = new Intent(MainActivity.this, SomeIntentService.class);
-                soapCallerIntent.putExtra(SomeIntentService.PARAM_IN_METHOD_ID, SomeIntentService.METHOD_ID_GET_ALL_MESSAGES);
+                Intent soapCallerIntent = new Intent(MainActivity.this, SoapCallerIntentService.class);
+                soapCallerIntent.putExtra(SoapCallerIntentService.PARAM_IN_METHOD_ID, SoapCallerIntentService.METHOD_ID_GET_ALL_MESSAGES);
                 startService(soapCallerIntent);
             }
         });
-//
-//        SomeIntentService soapCaller = new SomeIntentService();
-//        SoapRequestCreator soapRequestCreator = new SoapRequestCreator();
-//        String soapEnvelope = soapRequestCreator.returnGetAllMessagesRequest();
-////        String soapEnvelope = soapRequestCreator.returnGetMessageRequest(6);
-////        String soapEnvelope = soapRequestCreator.returnAddMessageRequest("Miau from Android", "El Kocurro");
-////        String soapEnvelope = soapRequestCreator.returnUpdateMessageRequest(9,"Miau EDITED from Android", "El Kocurro");
-////        String soapEnvelope = soapRequestCreator.returnRemoveMessageRequest(8);
-////        String soapEnvelope = soapRequestCreator.returnRemoveAllMessagesRequest();
-//        soapCaller.execute(soapEnvelope);
     }
-
     private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            String message = intent.getStringExtra(SomeIntentService.PARAM_OUT_SOAP_RESPONSE);
+            String message = intent.getStringExtra(SoapCallerIntentService.PARAM_OUT_SOAP_RESPONSE);
             textView.setText(message);
         }
     };
-
-//    private class SomeIntentService extends AsyncTask<String, Object, String> {
-//        public final static String PARAM_OUT_SOAP_RESPONSE = "soap-response";
-//        private HttpURLConnection connection;
-//        String result;
-//        @Override
-//        protected String doInBackground(String[] params) {
-//            initializeConnetion();
-//            sendRequest(params[0]);
-//            result = getResponse();
-//            return result;
-//        }
-//        private void initializeConnetion(){
-//            try {
-//                URL url = new URL("http://72249d3e.ngrok.io/SoapMessageService-1.0-SNAPSHOT/MessageService?wsdl");
-//                this.connection = (HttpURLConnection) url.openConnection();
-//                connection.setRequestMethod("GET");
-//                connection.setDoOutput(true);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        private void sendRequest(String request){
-//            try {
-//                DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-//                out.writeBytes(request);
-//                out.flush();
-//                out.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        private String getResponse(){
-//            StringBuffer response = null;
-//            try {
-//                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//                String inputLine;
-//                response = new StringBuffer();
-//                while ((inputLine = in.readLine()) != null) {
-//                    response.append(inputLine);
-//                }
-//                in.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            return response.toString();
-//        }
-//        @Override
-//        protected void onPostExecute(String o) {
-//            super.onPostExecute(o);
-//            SoapResponseParser responseParser = new SoapResponseParser();
-//            Object parsedResponse =  responseParser.parseResponse(result);
-//            String stringResponse = "";
-//            if(parsedResponse instanceof String)
-//                stringResponse = (String)parsedResponse;
-//            else if (parsedResponse instanceof List)
-//            {
-//                StringBuilder sb = new StringBuilder();
-//                for (Message messageToShow : (List<Message>)parsedResponse)
-//                {
-//                    sb.append(messageToShow.getId() + ": " + messageToShow.getMessageContent() + ": " + messageToShow.getAuthor() + ": " + messageToShow.getCreationDate()+"\n");
-//                }
-//                stringResponse = sb.toString();
-//            }
-//            Intent intent = new Intent(MainActivity.INTENT_FILTER_SOAP_CALLER);
-//            intent.putExtra(PARAM_OUT_SOAP_RESPONSE, stringResponse );
-//            LocalBroadcastManager.getInstance(MainActivity.this).sendBroadcast(intent);
-//        }
-//    }
 }
--- app/src/main/java/com/example/kamil/androidsoapclient2/activity/SoapCallerIntentService.java
+++ app/src/main/java/com/example/kamil/androidsoapclient2/activity/SoapCallerIntentService.java
@@ -4,38 +4,48 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
-
 import com.example.kamil.androidsoapclient2.buildingRequest.builder.SoapRequestCreator;
+import com.example.kamil.androidsoapclient2.model.Message;
+import com.example.kamil.androidsoapclient2.parsingResponse.SoapResponseParser;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
-public class SomeIntentService extends IntentService {
+public class SoapCallerIntentService extends IntentService {
     public final static String PARAM_OUT_SOAP_RESPONSE = "soap-response";
     public final static String PARAM_IN_METHOD_ID = "method-id";
     public final static int METHOD_ID_GET_ALL_MESSAGES = 1;
     public final static int METHOD_ID_GET_MESSAGE = 2;
     private HttpURLConnection connection;
-    String soapResponse;
-    SomeIntentService() {
-        super("SomeIntentService");
+    public SoapCallerIntentService() {
+        super("SoapCallerIntentService");
     }
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-        Log.e("SDDSFSDFFSD","UGABUGA");
         initializeConnetion();
         //TODO make message Parcelabe, so it can be passed in an intent
         //TODO rewrite getExtra code, so it can get necessary data
         int serviceMethodId = intent.getIntExtra(PARAM_IN_METHOD_ID, 0);
         String soapEnvelope = generateSoapEnvelope(serviceMethodId);
         sendRequest(soapEnvelope);
-        soapResponse = getResponse();
-        sendServiceResponseToActivity(soapResponse);
+        String rawSoapResponse = getResponse();
+        String parsedResponse = parseResponse(rawSoapResponse);
+        sendServiceResponseToActivity(parsedResponse);
+    }
+    private void initializeConnetion() {
+        try {
+            URL url = new URL("http://56763298.ngrok.io/SoapMessageService-1.0-SNAPSHOT/MessageService?wsdl");
+            this.connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     private String generateSoapEnvelope(int serviceMethodId) {
         String soapEnvelope;
@@ -51,21 +61,6 @@ public class SomeIntentService extends IntentService {
                 break;
         }
         return soapEnvelope;
-    }
-    private void sendServiceResponseToActivity(String soapResponse) {
-        Intent sendResponseIntent = new Intent(MainActivity.INTENT_FILTER_SOAP_CALLER);
-        sendResponseIntent.putExtra(PARAM_OUT_SOAP_RESPONSE, soapResponse);
-        LocalBroadcastManager.getInstance(this).sendBroadcast(sendResponseIntent);
-    }
-    private void initializeConnetion() {
-        try {
-            URL url = new URL("http://72249d3e.ngrok.io/SoapMessageService-1.0-SNAPSHOT/MessageService?wsdl");
-            this.connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setDoOutput(true);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
     private void sendRequest(String request) {
         try {
@@ -91,5 +86,27 @@ public class SomeIntentService extends IntentService {
             e.printStackTrace();
         }
         return response.toString();
+    }
+    private String parseResponse(String rawResponse){
+            SoapResponseParser responseParser = new SoapResponseParser();
+            Object parsedResponse =  responseParser.parseResponse(rawResponse);
+            String stringResponse = "";
+            if(parsedResponse instanceof String)
+                stringResponse = (String)parsedResponse;
+            else if (parsedResponse instanceof List)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (Message messageToShow : (List<Message>)parsedResponse)
+                {
+                    sb.append(messageToShow.getId() + ": " + messageToShow.getMessageContent() + ": " + messageToShow.getAuthor() + ": " + messageToShow.getCreationDate()+"\n");
+                }
+                stringResponse = sb.toString();
+            }
+            return stringResponse;
+    }
+    private void sendServiceResponseToActivity(String soapResponse) {
+        Intent sendResponseIntent = new Intent(MainActivity.INTENT_FILTER_SOAP_CALLER);
+        sendResponseIntent.putExtra(PARAM_OUT_SOAP_RESPONSE, soapResponse);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(sendResponseIntent);
     }
 }
